7341ffece895c70dd2fdfbf61de28169eef7a8e3
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-07-04 17:21:43 +0800

    sass官网看了一半入门。准备把剩下的看完，再在Vue中使用sass，结合slot实现表格复用

--- slot/src/components/son.vue
+++ slot/src/components/son.vue
@@ -3,6 +3,13 @@
       <slot name="head-slot"></slot>
       <h1>This is son</h1>
       <slot name="foot-slot"></slot>
+      <hr/>
+      <h1>下面的元素使用sass来控制样式</h1>
+      <div class="demo">
+          <ul>
+              <li></li>
+          </ul>
+      </div>
   </div>
 </template>
 
