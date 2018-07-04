45cd15cacf96fc2a2a1ce8fd2afbe14c8f76c178
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-07-04 15:48:24 +0800

    eslint的语法建议还真是强大。

---
+++ slot/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>This is App.vue</h1>
+    <app-son></app-son>
+  </div>
+</template>
+
+<script>
+import son from "./components/son.vue";
+
+export default {
+  components: {
+    "app-son": son
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ slot/src/components/son.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>This is son</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
