9e4af44d308674f7bc3cd0aa0df72479d732e2d3
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-07-04 16:38:46 +0800

    第一次使用sass，scss，变量可用。接下来使用scss的其它东东，测试用

--- slot/src/components/son.vue
+++ slot/src/components/son.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-        <slot name="head-slot"></slot>
-        <h1>This is son</h1>
-        <slot name="foot-slot"></slot>
-    </div>
+  <div>
+      <slot name="head-slot"></slot>
+      <h1>This is son</h1>
+      <slot name="foot-slot"></slot>
+  </div>
 </template>
 
 <script>
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+$col: #eeeeee;
+h2 {
+    color: $col;
+}
 </style>
