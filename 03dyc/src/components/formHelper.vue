667030b30648f3fda23e1575f9b31883680e1950
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-07-05 11:31:28 +0800

    使用git，将教程代码同步

---
+++ 03dyc/src/components/formHelper.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <slot name="form-header"></slot>
+        <slot name="form-p"></slot>
+        <label for="name">Name:</label>
+        <input type="text" name="name" placeholder="Please enter your name" />
+        <label for="phone">Phone</label>
+        <input type="text" name="phone" />
+        <slot name="form-footer"></slot>
+    </div>
+</template>
---
+++ 03dyc/src/components/table1.vue
@@ -0,0 +1,9 @@
+<template>
+<formHelper>
+    
+</formHelper>
+</template>
+
+<script>
+    
+</script>
